2032395f50119fcc82addbbd16d5200f000f6e70
Peter Keating <pete.keating@etchuk.com>
2020-06-13 18:25:06 +0100

    Upgrade RC2
    - Update target framework to 3.1
    - Update Orchard References
    - Update travis build script to target 3.1
    - Bump to 0.2.0-rc2

--- Manifest.cs
+++ Manifest.cs
@@ -4,6 +4,6 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "Host Theme",
     Author = "Etch UK Ltd.",
     Website = "https://etchuk.com",
-    Version = "0.1.1-rc1",
+    Version = "0.2.0-rc2",
     Description = "Theme for host site."
 )]
