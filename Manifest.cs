87022770ec3bc4e7342e5da85154f6b7c6881f0d
Peter Keating <pete.keating@etchuk.com>
2020-10-25 23:11:00 +0000

    Bump to v0.2.3

--- Manifest.cs
+++ Manifest.cs
@@ -4,6 +4,6 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "Host Theme",
     Author = "Etch UK Ltd.",
     Website = "https://etchuk.com",
-    Version = "0.2.2",
+    Version = "0.2.3",
     Description = "Theme for host site."
 )]
