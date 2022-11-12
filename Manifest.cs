e533a21056a2b0dcaeeb4cb02c197a861db1e846
Peter Keating <pete.keating@etchuk.com>
2022-11-12 09:33:51 +0000

    Use git tag to determine publish version

--- Manifest.cs
+++ Manifest.cs
@@ -4,6 +4,6 @@ using OrchardCore.DisplayManagement.Manifest;
     Name = "Host Theme",
     Author = "Etch UK Ltd.",
     Website = "https://etchuk.com",
-    Version = "1.3.0",
+    Version = "$(VersionNumber)",
     Description = "Theme for host site."
 )]
